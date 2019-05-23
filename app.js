fcd6f096a245f3e1eb9aeb72cde14f0a6f2fd152
piter20005 <piterst25@gmail.com>
2019-05-23 21:14:02 +0200

    HTML and CSS and JS ready

---
+++ app.js
@@ -0,0 +1,28 @@
+function addToDo(){
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes();
+    let input = document.getElementById("input_frame").value;
+    let list = document.getElementById("list");
+    let divCreator = document.createElement("div");
+    let text = document.createElement("p");
+    text.appendChild(document.createTextNode(input + time));
+    divCreator.appendChild(text);
+    divCreator.className = "object";
+    list.appendChild(divCreator);
+    document.getElementById("results").style.display = "flex";
+    divCreator.addEventListener("click",()=>{divCreator.className = "hideObject"});
+}
+
+function start(){
+    let name = document.getElementById("name").value;
+    let listName = document.getElementById("list_name").value;
+    if(name == "" || listName == ""){
+     alert("Wprowadź wymagane informacje")   
+    }else{
+        document.getElementById("login").style.display = "none";
+        document.getElementById("form").style.display = "flex";
+        document.getElementById("mylist").innerHTML = "Lista: " + listName;
+        document.getElementById("mylist2").innerHTML = "Użytkownika " + name;
+    }
+   
+}
